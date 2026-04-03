d8c20f6f582af01205261e8b9dd850321745282c
Rafa <rafa82001@gmail.com>
2026-04-03 19:34:41 +0200

    feat: add email logic

--- README.md
+++ README.md
@@ -10,8 +10,13 @@ El objetivo es crear una serie de tareas usando Clean Architecture con TypeScrip
 ```
 PORT=3000
 
+MAILER_SERVICE
 MAILER_EMAIL=
 MAILER_SECRET_KEY=
 
 PROD=false
 ```
+
+3. Ejecutar el comando `npm install`
+
+4. Ejecutar `npm run dev`
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,23 +1,33 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { sendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
+import { EmailService } from "./email/email.service";
 
 const fileSystemLogRepository = new LogRepositoryImpl(
   new FileSystemDataSource(),
 );
+const emailService = new EmailService();
+
 export class Server {
   public static start() {
-    console.log("Sever started...");
+    console.log("Server started...");
 
-    const url = "http://google.com";
-    CronService.createJob("*/5 * * * * *", () => {
-      new CheckService(
-        fileSystemLogRepository,
-        () => console.log(`${url} is ok`),
-        (error) => console.log(error),
-      ).execute(url);
-      // new CheckService().execute("http://localhost:3000");
-    });
+    // todo: Mandar email
+    // new sendEmailLogs(emailService, fileSystemLogRepository).execute([
+    //   "puercasocuellamos@gmail.com",
+    // ]);
+    // emailService.sendEmailWithFileSystemLogs(["puercasocuellamos@gmail.com"]);
+
+    // CronService.createJob("*/5 * * * * *", () => {
+    //   const url = "http://google.com";
+    //   new CheckService(
+    //     fileSystemLogRepository,
+    //     () => console.log(`${url} is ok`),
+    //     (error) => console.log(error),
+    //   ).execute(url);
+    //   // new CheckService().execute("http://localhost:3000");
+    // });
   }
 }
