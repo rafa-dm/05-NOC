33e7dcf0375f1016e5518d3ed47b80f450ed7f81
Rafa <rafa82001@gmail.com>
2026-04-03 13:14:36 +0200

    feat: implement logging system

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,13 +1,19 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 
+const fileSystemLogRepository = new LogRepositoryImpl(
+  new FileSystemDataSource(),
+);
 export class Server {
   public static start() {
     console.log("Sever started...");
 
-    const url = "https://google.com";
+    const url = "http://google.com";
     CronService.createJob("*/5 * * * * *", () => {
       new CheckService(
+        fileSystemLogRepository,
         () => console.log(`${url} is ok`),
         (error) => console.log(error),
       ).execute(url);
